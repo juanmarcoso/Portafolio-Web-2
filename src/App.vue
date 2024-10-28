c1e319e7f67ec16fb54c0e17fcb3987a6f95a08a
Juan Marcos Orellana <juanmarcoso@gmail.com>
2024-10-28 10:43:13 -0300

    Agregando clase 8 y parte 9

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,9 @@
 <script setup>
-import NavBar from './components/icons/NavBar.vue';
-import DatosPersonales from './components/icons/DatosPersonales.vue';
-import EducacionComponente from './components/icons/EducacionComponente.vue';
-import ExperienciaComponente from './components/icons/ExperienciaComponente.vue';
-import ProyectoComponente from './components/icons/ProyectoComponente.vue';
+import NavBar from './components/NavBar.vue';
+import DatosPersonales from './components/DatosPersonales.vue';
+import EducacionComponente from './components/EducacionComponente.vue';
+import ExperienciaComponente from './components/ExperienciaComponente.vue';
+import ProyectoComponente from './components/ProyectoComponente.vue';
 </script>
 
 <template>
@@ -17,14 +17,17 @@ import ProyectoComponente from './components/icons/ProyectoComponente.vue';
     <!-- El estilo css de section se encuentra en base.css -->
     <section id="educacion">
       <h2> Educacion - Cursos </h2>
+      <!-- Componente Educacion -->
       <EducacionComponente />
     </section>
     <section id="experiencia">
       <h2> Experiencia </h2>
+      <!-- Componente Experiencia -->
       <ExperienciaComponente />
     </section>
     <section id="proyectos">
       <h2> Proyectos </h2>
+      <!-- Componente Proyectos -->
       <ProyectoComponente />
     </section>
     <section id="habilidades">
